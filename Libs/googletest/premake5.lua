43fc3ca3bc4b55b5e2c8b7be32d03a4c6e0cacbb
brunopj <brunopj1@hotmail.com>
2024-07-19 15:03:35 +0100

    added gtest dependency

---
+++ Libs/googletest/premake5.lua
@@ -0,0 +1,17 @@
+project "Google-Test"
+	kind "StaticLib"
+	language "C++"
+
+	targetdir ("../../Bin/%{cfg.buildcfg}/%{cfg.platform}/%{prj.name}")
+	objdir    ("../../Obj/%{cfg.buildcfg}/%{cfg.platform}/%{prj.name}")
+
+    includedirs { 
+		"googletest/googletest/include/",
+		"googletest/googletest/"
+	}
+
+	files {
+        "premake5.lua",
+		"googletest/googletest/include/**.h",
+		"googletest/googletest/src/*.cc"
+	}
