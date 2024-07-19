1d009332af059e8d345f884925dba123421b4572
brunopj <brunopj1@hotmail.com>
2024-07-19 22:45:29 +0100

    testing pipeline with compilation errors

--- Playground/AudioPlayground/Source/Entities/WindowConstants.h
+++ Playground/AudioPlayground/Source/Entities/WindowConstants.h
@@ -1,12 +1,8 @@
 ﻿#pragma once
 
-#ifdef DEBUG
-
 #include <glm/vec2.hpp>
 
 constexpr glm::vec2 audio_window_size = {250.0f, 75.0f};
 constexpr glm::vec2 audio_player_window_size = {250.0f, 270.0f};
 
 constexpr glm::vec2 window_padding = {10.0f, 10.0f};
-
-#endif
--- Playground/AudioPlayground/Source/Main.cpp
+++ Playground/AudioPlayground/Source/Main.cpp
@@ -2,9 +2,13 @@
 #include <GameMode/AudioPlaygroundGameMode.h>
 #include "Util/MemoryLeakDetection.h"
 
+#ifndef DEBUG
+static_assert(false, "The Playground projects are only available in Debug configurations");
+#endif
+
 i32 main()
 {
-    TGL::ApplicationConfig config;
+    TGL::ApplicationConfig config
     config.WindowTitle = "TrianGL - AudioPlayground";
     config.Vsync = true;
 
